2abb74cb2f972880c5fad59d4d7ae73f59b5f105
duzaoDev <edrsilvadev@gmail.com>
2025-08-15 03:24:27 -0300

    att: tentativa frustrada de integração ranking <-> database

---
+++ app/src/main/java/com/example/vcquizo/view/model/RankingViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.vcquizo.view.model
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.vcquizo.data.UserRepository
+import com.example.vcquizo.domain.model.User
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class RankingViewModel(application: Application) : AndroidViewModel(application) {
+    private val userRepository = UserRepository(application)
+
+    private val _rankingList = MutableStateFlow<List<User>>(emptyList())
+    val rankingList: StateFlow<List<User>> = _rankingList.asStateFlow()
+
+    init {
+        fetchRanking()
+    }
+
+    private fun fetchRanking() {
+        viewModelScope.launch{
+            val users = userRepository.getRanking()
+            _rankingList.value = users
+        }
+
+    }
+
+}
